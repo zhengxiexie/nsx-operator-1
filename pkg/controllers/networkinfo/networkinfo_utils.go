vmware-tanzu/nsx-operator

Replace vpc controller with networkinfo controller
Signed-off-by: Xie Zheng <xizheng@vmware.com>

pkg/controllers/networkinfo/networkinfo_utils.go
@@ -1,0 +1,89 @@
+package networkinfo
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+
+	v1 "k8s.io/api/core/v1"
+	apitypes "k8s.io/apimachinery/pkg/types"
+	client "sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/vmware-tanzu/nsx-operator/pkg/apis/v1alpha1"
+
+	"github.com/vmware-tanzu/nsx-operator/pkg/controllers/common"
+	"github.com/vmware-tanzu/nsx-operator/pkg/metrics"
+)
+
+func deleteFail(r *NetworkInfoReconciler, c *context.Context, o *v1alpha1.NetworkInfo, e *error, client client.Client) {
+	createdVpc := &v1alpha1.VPCState{}
+	setNetworkInfoVPCStatus(c, o, client, createdVpc)
+	r.Recorder.Event(o, v1.EventTypeWarning, common.ReasonFailDelete, fmt.Sprintf("%v", *e))
+	metrics.CounterInc(r.Service.NSXConfig, metrics.ControllerDeleteFailTotal, common.MetricResTypeVPC)
+}
+
+func updateFail(r *NetworkInfoReconciler, c *context.Context, o *v1alpha1.NetworkInfo, e *error, client client.Client) {
+	createdVpc := &v1alpha1.VPCState{}
+	setNetworkInfoVPCStatus(c, o, client, createdVpc)
+	r.Recorder.Event(o, v1.EventTypeWarning, common.ReasonFailUpdate, fmt.Sprintf("%v", *e))
+	metrics.CounterInc(r.Service.NSXConfig, metrics.ControllerUpdateFailTotal, MetricResType)
+}
+
+func updateSuccess(r *NetworkInfoReconciler, c *context.Context, o *v1alpha1.NetworkInfo, client client.Client,
+	name string, path string, snatIP string, subnetPath string, cidr string, privateCidrs []string, ncName string) {
+	createdVpc := &v1alpha1.VPCState{
+		Name:                    name,
+		VPCPath:                 path,
+		DefaultSNATIP:           snatIP,
+		LoadBalancerIPAddresses: cidr,
+		PrivateIPv4CIDRs:        privateCidrs,
+	}
+	setNetworkInfoVPCStatus(c, o, client, createdVpc)
+	// ako needs to know the avi subnet path created by nsx
+	setVPCNetworkConfigurationStatus(c, client, ncName, name, subnetPath)
+	r.Recorder.Event(o, v1.EventTypeNormal, common.ReasonSuccessfulUpdate, "NetworkInfo CR has been successfully updated")
+	metrics.CounterInc(r.Service.NSXConfig, metrics.ControllerUpdateSuccessTotal, common.MetricResTypeVPC)
+}
+
+func deleteSuccess(r *NetworkInfoReconciler, _ *context.Context, o *v1alpha1.NetworkInfo) {
+	r.Recorder.Event(o, v1.EventTypeNormal, common.ReasonSuccessfulDelete, "NetworkInfo CR has been successfully deleted")
+	metrics.CounterInc(r.Service.NSXConfig, metrics.ControllerDeleteSuccessTotal, common.MetricResTypeVPC)
+}
+
+func setNetworkInfoVPCStatus(ctx *context.Context, networkInfo *v1alpha1.NetworkInfo, client client.Client, createdVPC *v1alpha1.VPCState) {
+	existingVPC := &v1alpha1.VPCState{}
+	if len(networkInfo.VPCs) > 0 {
+		existingVPC = &networkInfo.VPCs[0]
+	}
+	if !reflect.DeepEqual(*existingVPC, *createdVPC) {
+		networkInfo.VPCs = []v1alpha1.VPCState{*createdVPC}
+		client.Update(*ctx, networkInfo)
+	}
+}
+
+func setVPCNetworkConfigurationStatus(ctx *context.Context, client client.Client, ncName string, vpcName string, aviSubnetPath string) {
+	// read v1alpha1.VPCNetworkConfiguration by ncName
+	nc := &v1alpha1.VPCNetworkConfiguration{}
+	err := client.Get(*ctx, apitypes.NamespacedName{Name: ncName}, nc)
+	if err != nil {
+		log.Error(err, "failed to get VPCNetworkConfiguration", "Name", ncName)
+	}
+	createdVPCInfo := &v1alpha1.VPCInfo{
+		Name:            vpcName,
+		AVISESubnetPath: aviSubnetPath,
+	}
+	// iterate through VPCNetworkConfiguration.Status.VPCs, if vpcName already exists, update it
+	for i, vpc := range nc.Status.VPCs {
+		if vpc.Name == vpcName {
+			nc.Status.VPCs[i] = *createdVPCInfo
+			client.Status().Update(*ctx, nc)
+			return
+		}
+	}
+	// else append the new VPCInfo
+	if nc.Status.VPCs == nil {
+		nc.Status.VPCs = []v1alpha1.VPCInfo{}
+	}
+	nc.Status.VPCs = append(nc.Status.VPCs, *createdVPCInfo)
+	client.Status().Update(*ctx, nc)
+}

pkg/controllers/networkinfo/vpcnetworkconfig_handler.go
@@ -1,13 +1,17 @@
-package vpc
+package networkinfo
 
 import (
 	"context"
+	"errors"
+	"strconv"
+	"strings"
 
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	"github.com/vmware-tanzu/nsx-operator/pkg/apis/v1alpha1"
-	"github.com/vmware-tanzu/nsx-operator/pkg/nsx/services/vpc"
+
+	commontypes "github.com/vmware-tanzu/nsx-operator/pkg/nsx/services/common"
 
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/util/workqueue"
@@ -22,7 +26,7 @@ import (
 
 type VPCNetworkConfigurationHandler struct {
 	Client     client.Client
-	vpcService *vpc.VPCService
+	vpcService commontypes.VPCServiceProvider
 }
 
 func (h *VPCNetworkConfigurationHandler) Create(_ context.Context, e event.CreateEvent, _ workqueue.RateLimitingInterface) {
@@ -46,7 +50,7 @@ func (h *VPCNetworkConfigurationHandler) Generic(_ context.Context, _ event.Gene
 	log.V(1).Info("VPCNetworkConfiguration generic event, do nothing")
 }
 
-func (h *VPCNetworkConfigurationHandler) Update(_ context.Context, e event.UpdateEvent, q workqueue.RateLimitingInterface) {
+func (h *VPCNetworkConfigurationHandler) Update(ctx context.Context, e event.UpdateEvent, q workqueue.RateLimitingInterface) {
 	log.V(1).Info("start processing VPC network config update event")
 	oldNc := e.ObjectOld.(*v1alpha1.VPCNetworkConfiguration)
 	newNc := e.ObjectNew.(*v1alpha1.VPCNetworkConfiguration)
@@ -66,22 +70,20 @@ func (h *VPCNetworkConfigurationHandler) Update(_ context.Context, e event.Updat
 	h.vpcService.RegisterVPCNetworkConfig(newNc.Name, *info)
 
 	nss := h.vpcService.GetNamespacesByNetworkconfigName(newNc.Name)
-	ctx := context.Background()
-	// find vpcs under each ns, and reconcile the vpc object
 	for _, ns := range nss {
-		vpcList := &v1alpha1.VPCList{}
-		err := h.Client.List(ctx, vpcList, client.InNamespace(ns))
+		networkInfoList := &v1alpha1.NetworkInfoList{}
+		err := h.Client.List(ctx, networkInfoList, client.InNamespace(ns))
 		if err != nil {
 			log.Error(err, "failed to list VPCs in namespace", "Namespace", ns)
 			continue
 		}
 
-		for _, vpc := range vpcList.Items {
-			log.Info("reconcile VPC CR due to modifying network config CR", "VPC", vpc.Name, "Namespace", ns, "NetworkConfig", newNc.Name)
+		for _, networkInfo := range networkInfoList.Items {
+			log.Info("reconcile NetworkInfo CR due to modifying network config CR", "NetworkInfo", networkInfo.Name, "Namespace", ns, "NetworkConfig", newNc.Name)
 			q.Add(reconcile.Request{
 				NamespacedName: types.NamespacedName{
-					Name:      vpc.Name,
-					Namespace: vpc.Namespace,
+					Name:      networkInfo.Name,
+					Namespace: networkInfo.Namespace,
 				},
 			})
 		}
@@ -109,4 +111,51 @@ func getListSize(s []string) int {
 	} else {
 		return len(s)
 	}
+}
+
+func buildNetworkConfigInfo(vpcConfigCR v1alpha1.VPCNetworkConfiguration) (*commontypes.VPCNetworkConfigInfo, error) {
+	org, project, err := nsxtProjectPathToId(vpcConfigCR.Spec.NSXTProject)
+	if err != nil {
+		log.Error(err, "failed to parse nsx-t project in network config", "Project Path", vpcConfigCR.Spec.NSXTProject)
+		return nil, err
+	}
+
+	ninfo := &commontypes.VPCNetworkConfigInfo{
+		IsDefault:               isDefaultNetworkConfigCR(vpcConfigCR),
+		Org:                     org,
+		Name:                    vpcConfigCR.Name,
+		DefaultGatewayPath:      vpcConfigCR.Spec.DefaultGatewayPath,
+		EdgeClusterPath:         vpcConfigCR.Spec.EdgeClusterPath,
+		NsxtProject:             project,
+		ExternalIPv4Blocks:      vpcConfigCR.Spec.ExternalIPv4Blocks,
+		PrivateIPv4CIDRs:        vpcConfigCR.Spec.PrivateIPv4CIDRs,
+		DefaultIPv4SubnetSize:   vpcConfigCR.Spec.DefaultIPv4SubnetSize,
+		DefaultSubnetAccessMode: vpcConfigCR.Spec.DefaultSubnetAccessMode,
+		ShortID:                 vpcConfigCR.Spec.ShortID,
+	}
+	return ninfo, nil
+}
+
+func isDefaultNetworkConfigCR(vpcConfigCR v1alpha1.VPCNetworkConfiguration) bool {
+	annos := vpcConfigCR.GetAnnotations()
+	val, exist := annos[commontypes.AnnotationDefaultNetworkConfig]
+	if exist {
+		boolVar, err := strconv.ParseBool(val)
+		if err != nil {
+			log.Error(err, "failed to parse annotation to check default NetworkConfig", "Annotation", annos[commontypes.AnnotationDefaultNetworkConfig])
+			return false
+		}
+		return boolVar
+	}
+	return false
+}
+
+// parse org id and project id from nsxtProject path
+// example /orgs/default/projects/nsx_operator_e2e_test
+func nsxtProjectPathToId(path string) (string, string, error) {
+	parts := strings.Split(path, "/")
+	if len(parts) < 4 {
+		return "", "", errors.New("invalid NSXT project path")
+	}
+	return parts[2], parts[len(parts)-1], nil
 }

zhengxiexie/nsx-operator-1

pkg/controllers/networkinfo/networkinfo_utils.go
@@ -1,0 +1,89 @@
+package networkinfo
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+
+	v1 "k8s.io/api/core/v1"
+	apitypes "k8s.io/apimachinery/pkg/types"
+	client "sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/vmware-tanzu/nsx-operator/pkg/apis/v1alpha1"
+
+	"github.com/vmware-tanzu/nsx-operator/pkg/controllers/common"
+	"github.com/vmware-tanzu/nsx-operator/pkg/metrics"
+)
+
+func deleteFail(r *NetworkInfoReconciler, c *context.Context, o *v1alpha1.NetworkInfo, e *error, client client.Client) {
+	createdVpc := &v1alpha1.VPCState{}
+	setNetworkInfoVPCStatus(c, o, client, createdVpc)
+	r.Recorder.Event(o, v1.EventTypeWarning, common.ReasonFailDelete, fmt.Sprintf("%v", *e))
+	metrics.CounterInc(r.Service.NSXConfig, metrics.ControllerDeleteFailTotal, common.MetricResTypeNetworkInfo)
+}
+
+func updateFail(r *NetworkInfoReconciler, c *context.Context, o *v1alpha1.NetworkInfo, e *error, client client.Client) {
+	createdVpc := &v1alpha1.VPCState{}
+	setNetworkInfoVPCStatus(c, o, client, createdVpc)
+	r.Recorder.Event(o, v1.EventTypeWarning, common.ReasonFailUpdate, fmt.Sprintf("%v", *e))
+	metrics.CounterInc(r.Service.NSXConfig, metrics.ControllerUpdateFailTotal, MetricResType)
+}
+
+func updateSuccess(r *NetworkInfoReconciler, c *context.Context, o *v1alpha1.NetworkInfo, client client.Client,
+	name string, path string, snatIP string, subnetPath string, cidr string, privateCidrs []string, ncName string) {
+	createdVpc := &v1alpha1.VPCState{
+		Name:                    name,
+		VPCPath:                 path,
+		DefaultSNATIP:           snatIP,
+		LoadBalancerIPAddresses: cidr,
+		PrivateIPv4CIDRs:        privateCidrs,
+	}
+	setNetworkInfoVPCStatus(c, o, client, createdVpc)
+	// ako needs to know the avi subnet path created by nsx
+	setVPCNetworkConfigurationStatus(c, client, ncName, name, subnetPath)
+	r.Recorder.Event(o, v1.EventTypeNormal, common.ReasonSuccessfulUpdate, "NetworkInfo CR has been successfully updated")
+	metrics.CounterInc(r.Service.NSXConfig, metrics.ControllerUpdateSuccessTotal, common.MetricResTypeNetworkInfo)
+}
+
+func deleteSuccess(r *NetworkInfoReconciler, _ *context.Context, o *v1alpha1.NetworkInfo) {
+	r.Recorder.Event(o, v1.EventTypeNormal, common.ReasonSuccessfulDelete, "NetworkInfo CR has been successfully deleted")
+	metrics.CounterInc(r.Service.NSXConfig, metrics.ControllerDeleteSuccessTotal, common.MetricResTypeNetworkInfo)
+}
+
+func setNetworkInfoVPCStatus(ctx *context.Context, networkInfo *v1alpha1.NetworkInfo, client client.Client, createdVPC *v1alpha1.VPCState) {
+	existingVPC := &v1alpha1.VPCState{}
+	if len(networkInfo.VPCs) > 0 {
+		existingVPC = &networkInfo.VPCs[0]
+	}
+	if !reflect.DeepEqual(*existingVPC, *createdVPC) {
+		networkInfo.VPCs = []v1alpha1.VPCState{*createdVPC}
+		client.Update(*ctx, networkInfo)
+	}
+}
+
+func setVPCNetworkConfigurationStatus(ctx *context.Context, client client.Client, ncName string, vpcName string, aviSubnetPath string) {
+	// read v1alpha1.VPCNetworkConfiguration by ncName
+	nc := &v1alpha1.VPCNetworkConfiguration{}
+	err := client.Get(*ctx, apitypes.NamespacedName{Name: ncName}, nc)
+	if err != nil {
+		log.Error(err, "failed to get VPCNetworkConfiguration", "Name", ncName)
+	}
+	createdVPCInfo := &v1alpha1.VPCInfo{
+		Name:            vpcName,
+		AVISESubnetPath: aviSubnetPath,
+	}
+	// iterate through VPCNetworkConfiguration.Status.VPCs, if vpcName already exists, update it
+	for i, vpc := range nc.Status.VPCs {
+		if vpc.Name == vpcName {
+			nc.Status.VPCs[i] = *createdVPCInfo
+			client.Status().Update(*ctx, nc)
+			return
+		}
+	}
+	// else append the new VPCInfo
+	if nc.Status.VPCs == nil {
+		nc.Status.VPCs = []v1alpha1.VPCInfo{}
+	}
+	nc.Status.VPCs = append(nc.Status.VPCs, *createdVPCInfo)
+	client.Status().Update(*ctx, nc)
+}

pkg/controllers/networkinfo/vpcnetworkconfig_handler.go
@@ -1,13 +1,17 @@
-package vpc
+package networkinfo
 
 import (
 	"context"
+	"errors"
+	"strconv"
+	"strings"
 
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	"github.com/vmware-tanzu/nsx-operator/pkg/apis/v1alpha1"
-	"github.com/vmware-tanzu/nsx-operator/pkg/nsx/services/vpc"
+
+	commontypes "github.com/vmware-tanzu/nsx-operator/pkg/nsx/services/common"
 
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/util/workqueue"
@@ -22,7 +26,7 @@ import (
 
 type VPCNetworkConfigurationHandler struct {
 	Client     client.Client
-	vpcService *vpc.VPCService
+	vpcService commontypes.VPCServiceProvider
 }
 
 func (h *VPCNetworkConfigurationHandler) Create(_ context.Context, e event.CreateEvent, _ workqueue.RateLimitingInterface) {
@@ -46,7 +50,7 @@ func (h *VPCNetworkConfigurationHandler) Generic(_ context.Context, _ event.Gene
 	log.V(1).Info("VPCNetworkConfiguration generic event, do nothing")
 }
 
-func (h *VPCNetworkConfigurationHandler) Update(_ context.Context, e event.UpdateEvent, q workqueue.RateLimitingInterface) {
+func (h *VPCNetworkConfigurationHandler) Update(ctx context.Context, e event.UpdateEvent, q workqueue.RateLimitingInterface) {
 	log.V(1).Info("start processing VPC network config update event")
 	oldNc := e.ObjectOld.(*v1alpha1.VPCNetworkConfiguration)
 	newNc := e.ObjectNew.(*v1alpha1.VPCNetworkConfiguration)
@@ -66,22 +70,20 @@ func (h *VPCNetworkConfigurationHandler) Update(_ context.Context, e event.Updat
 	h.vpcService.RegisterVPCNetworkConfig(newNc.Name, *info)
 
 	nss := h.vpcService.GetNamespacesByNetworkconfigName(newNc.Name)
-	ctx := context.Background()
-	// find vpcs under each ns, and reconcile the vpc object
 	for _, ns := range nss {
-		vpcList := &v1alpha1.VPCList{}
-		err := h.Client.List(ctx, vpcList, client.InNamespace(ns))
+		networkInfos := &v1alpha1.NetworkInfoList{}
+		err := h.Client.List(ctx, networkInfos, client.InNamespace(ns))
 		if err != nil {
 			log.Error(err, "failed to list VPCs in namespace", "Namespace", ns)
 			continue
 		}
 
-		for _, vpc := range vpcList.Items {
-			log.Info("reconcile VPC CR due to modifying network config CR", "VPC", vpc.Name, "Namespace", ns, "NetworkConfig", newNc.Name)
+		for _, networkInfo := range networkInfos.Items {
+			log.Info("reconcile NetworkInfo CR due to modifying network config CR", "NetworkInfo", networkInfo.Name, "Namespace", ns, "NetworkConfig", newNc.Name)
 			q.Add(reconcile.Request{
 				NamespacedName: types.NamespacedName{
-					Name:      vpc.Name,
-					Namespace: vpc.Namespace,
+					Name:      networkInfo.Name,
+					Namespace: networkInfo.Namespace,
 				},
 			})
 		}
@@ -109,4 +111,51 @@ func getListSize(s []string) int {
 	} else {
 		return len(s)
 	}
+}
+
+func buildNetworkConfigInfo(vpcConfigCR v1alpha1.VPCNetworkConfiguration) (*commontypes.VPCNetworkConfigInfo, error) {
+	org, project, err := nsxtProjectPathToId(vpcConfigCR.Spec.NSXTProject)
+	if err != nil {
+		log.Error(err, "failed to parse nsx-t project in network config", "Project Path", vpcConfigCR.Spec.NSXTProject)
+		return nil, err
+	}
+
+	ninfo := &commontypes.VPCNetworkConfigInfo{
+		IsDefault:               isDefaultNetworkConfigCR(vpcConfigCR),
+		Org:                     org,
+		Name:                    vpcConfigCR.Name,
+		DefaultGatewayPath:      vpcConfigCR.Spec.DefaultGatewayPath,
+		EdgeClusterPath:         vpcConfigCR.Spec.EdgeClusterPath,
+		NsxtProject:             project,
+		ExternalIPv4Blocks:      vpcConfigCR.Spec.ExternalIPv4Blocks,
+		PrivateIPv4CIDRs:        vpcConfigCR.Spec.PrivateIPv4CIDRs,
+		DefaultIPv4SubnetSize:   vpcConfigCR.Spec.DefaultIPv4SubnetSize,
+		DefaultSubnetAccessMode: vpcConfigCR.Spec.DefaultSubnetAccessMode,
+		ShortID:                 vpcConfigCR.Spec.ShortID,
+	}
+	return ninfo, nil
+}
+
+func isDefaultNetworkConfigCR(vpcConfigCR v1alpha1.VPCNetworkConfiguration) bool {
+	annos := vpcConfigCR.GetAnnotations()
+	val, exist := annos[commontypes.AnnotationDefaultNetworkConfig]
+	if exist {
+		boolVar, err := strconv.ParseBool(val)
+		if err != nil {
+			log.Error(err, "failed to parse annotation to check default NetworkConfig", "Annotation", annos[commontypes.AnnotationDefaultNetworkConfig])
+			return false
+		}
+		return boolVar
+	}
+	return false
+}
+
+// parse org id and project id from nsxtProject path
+// example /orgs/default/projects/nsx_operator_e2e_test
+func nsxtProjectPathToId(path string) (string, string, error) {
+	parts := strings.Split(path, "/")
+	if len(parts) < 4 {
+		return "", "", errors.New("invalid NSXT project path")
+	}
+	return parts[2], parts[len(parts)-1], nil
 }